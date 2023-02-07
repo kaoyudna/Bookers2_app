38e6b4935ec22c7d96d53d84900baa2b2cdc13ce
EC2 Default User <ec2-user@ip-172-31-43-118.ap-northeast-1.compute.internal>
2023-02-07 08:56:51 +0000

    [Add]フォロー機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only:[:create,:destroy]
+    get 'relationships#followers'=>'relationships#followers',as: 'follower'
+    get 'relationships/followings'=>'relationships#followings',as: 'following'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
